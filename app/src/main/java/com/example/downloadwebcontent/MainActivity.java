0e75d11011ae54061a744ec9e66c7779efa105bd
homna <nazari_homeira@yahoo.com>
2019-02-05 20:05:27 +0330

    Download HTML file for two URLs.

--- app/src/main/java/com/example/downloadwebcontent/MainActivity.java
+++ app/src/main/java/com/example/downloadwebcontent/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.downloadwebcontent;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DownloadWebSite downloadWebSite = new DownloadWebSite();
+        String output = "";
+        try {
+            output = downloadWebSite.execute(new String[]{"https://zappycode.com", "http://www.browxy.com"}).get();
+        } catch (Exception e) {
+            Log.i("Problem2", e.getMessage());
+        }
+        Log.i("HTML", output);
+    }
+
+    public class DownloadWebSite extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            StringBuilder result = new StringBuilder();
+            result.append("");
+            for (String string : strings) {
+                Log.i("HTML", "Downloading ...." + string);
+                try {
+                    URL url = new URL(string);
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                    InputStream inputStream = httpURLConnection.getInputStream();
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                    //First Solution
+                    int read = inputStreamReader.read();
+                    while (read != -1) {
+                        char character = (char) read;
+                        result.append(character);
+
+                        read = inputStreamReader.read();
+                    }
+                } catch (Exception e) {
+                    Log.i("Problem1", e.getMessage());
+                }
+            }
+            return result.toString();
+        }
     }
 }
